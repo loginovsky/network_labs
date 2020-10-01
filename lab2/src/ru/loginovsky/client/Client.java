1c3315c8b4d516282217c4ad18f7a993301caf94
loginovsky <1stbender@gmail.com>
2020-10-02 03:00:34 +0700

    lab2

---
+++ lab2/src/ru/loginovsky/client/Client.java
@@ -0,0 +1,57 @@
+package ru.loginovsky.client;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    private final static int BUF_SIZE = 4096;
+    private final static int ANSWER_SIZE = 1;
+    private final static byte YES_ANSWER = 1;
+    private final static char SEPARATOR = '/';
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java Client \"path to file\" \"server address\" \"server port\"");
+        }
+        String host = args[1];
+        int port = Integer.parseInt(args[2]);
+        File file = new File(args[0]);
+        if (!file.canRead()) {
+            System.out.println("Cannot read file " + args[0]);
+            return;
+        }
+
+        try (InputStream fileIn = new FileInputStream(file);
+                Socket socket = new Socket(host, port)) {
+            byte[] buf = new byte[BUF_SIZE];
+            OutputStream socketOut = socket.getOutputStream();
+            InputStream socketIn = socket.getInputStream();
+            byte[] header = (file.getName() + SEPARATOR + file.length() + SEPARATOR).getBytes();
+            socketOut.write(header);
+            byte[] answer = new byte[ANSWER_SIZE];
+            socketIn.read(answer);
+            if (answer[0] != YES_ANSWER) {
+                return;
+            }
+            int bytesCount;
+            while ((bytesCount = fileIn.read(buf)) != -1) {
+                socketOut.write(buf,0,bytesCount);
+            }
+            socket.shutdownOutput();
+            byte[] successAnswer = new byte[ANSWER_SIZE];
+            socketIn.read(successAnswer);
+            if (successAnswer[0] == YES_ANSWER) {
+                System.out.println("Upload completed successfully");
+            } else {
+                System.out.println("Upload was unsuccessful");
+            }
+            socketIn.close();
+            socketOut.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File " + args[0] + " does not exist");
+        } catch (IOException e) {
+            System.err.println("Cannot connect to host " + host + " : " + e.getMessage());
+        }
+    }
+
+}
---
+++ lab2/src/ru/loginovsky/server/Server.java
@@ -0,0 +1,140 @@
+package ru.loginovsky.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    private final static long SIZE_LIMIT = 1_000_000_000_000L;
+    private final static int ANSWER_SIZE = 1;
+    private final static String UPLOADS_DIRNAME = "uploads";
+    private final static long TIME_INTERVAL = 3_000_000_000L;
+    public static void main(String[] args) {
+        String pwd = System.getProperty("user.dir");
+        File uploadsDir = new File(pwd, UPLOADS_DIRNAME);
+        uploadsDir.mkdir();
+        if (args.length < 1) {
+            System.out.println("Enter port number for a server");
+            return;
+        }
+        int socketPort = Integer.parseInt(args[0]);
+        try {
+            ServerSocket serverSocket = new ServerSocket(socketPort);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                getConnectionThread(socket, uploadsDir).start();
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+    static Thread getConnectionThread(Socket socket, File uploadsDir) {
+        return new Thread(()-> {
+            try {
+                String[] header = readHeader(socket);
+                String filename = formatFilename(header[0], uploadsDir);
+                long filesize = Long.parseLong(header[1]);
+                boolean accepted = fileAccepted(filename, filesize, uploadsDir);
+                sendAnswer(socket, accepted);
+                if (!accepted) {
+                    return;
+                }
+                long readBytes = readFile(socket, filename, uploadsDir);
+                boolean success = (readBytes == filesize);
+                sendAnswer(socket, success);
+                if (success) {
+                    System.out.println(filename + " successfully uploaded to " + uploadsDir.getPath());
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        });
+    }
+
+    static String[] readHeader(Socket socket) throws IOException {
+        InputStream socketIn = socket.getInputStream();
+        int bufSize = socket.getReceiveBufferSize();
+        System.out.println(bufSize);
+        byte[] buf = new byte[bufSize];
+        int bytesCount;
+        StringBuilder str = new StringBuilder();
+        while ((bytesCount = socketIn.read(buf)) != -1) {
+            str.append(new String(buf, 0, bytesCount, StandardCharsets.UTF_8));
+            if(str.toString().matches("^.+\\/\\d+\\/$")) {
+                break;
+            }
+        }
+        return str.toString().split("\\/");
+    }
+    static boolean fileAccepted(String filename, long filesize, File uploadsDir) {
+        long freeSpace = uploadsDir.getUsableSpace();
+        System.out.println(freeSpace);
+        return filesize < freeSpace && filesize < SIZE_LIMIT;
+    }
+    static void sendAnswer(Socket socket, boolean accepted) throws IOException{
+        OutputStream socketOut = socket.getOutputStream();
+        byte[] answer = new byte[ANSWER_SIZE];
+        answer[0] = accepted ? (byte)1 : (byte)0;
+        socketOut.write(answer);
+    }
+    static long readFile(Socket socket, String filename, File uploadsDir) throws IOException {
+        InputStream socketIn = socket.getInputStream();
+        int bufSize = socket.getReceiveBufferSize();
+        System.out.println(bufSize);
+        byte[] buf = new byte[bufSize];
+        File file = new File(uploadsDir, filename);
+        OutputStream fileOut = new FileOutputStream(file);
+        int bytesCount = 0;
+        int lastBytes = 0;
+        long totalBytesCount = 0L;
+        long beforeTime = System.nanoTime();
+        long startIntervalTime = 0L, startTime = 0L, finishTime = TIME_INTERVAL + 1;
+        boolean statsPrinted = false;
+        boolean firstTime = true;
+        while (bytesCount != -1) {
+            if (firstTime) {
+                startIntervalTime = System.nanoTime();
+                firstTime = false;
+            } else if (finishTime - startIntervalTime > TIME_INTERVAL) {
+                System.out.println(filename);
+                System.out.println("Current speed: " + ((long)bytesCount * 1_000_000L / (finishTime - startTime)) + " Kb/s");
+                System.out.println("Average speed: " + (totalBytesCount * 1_000_000L / (finishTime - beforeTime)) + " Kb/s");
+                System.out.println();
+                startIntervalTime = System.nanoTime();
+                statsPrinted = true;
+            }
+            startTime = System.nanoTime();
+            bytesCount = socketIn.read(buf);
+            finishTime = System.nanoTime();
+            if (bytesCount != -1) {
+                //System.out.println("inside");
+                fileOut.write(buf, 0, bytesCount);
+                totalBytesCount += bytesCount;
+                lastBytes = bytesCount;
+            }
+        }
+        if(!statsPrinted) {
+            System.out.println(filename);
+            System.out.println("Current speed: " + ((long) lastBytes * 1_000_000L / (finishTime - startTime)) + " Kb/s");
+            System.out.println("Average speed: " + (totalBytesCount * 1_000_000L / (finishTime - beforeTime)) + " Kb/s");
+            System.out.println();
+        }
+        fileOut.close();
+        return totalBytesCount;
+    }
+    static String formatFilename(String filename, File uploadsDir) {
+        String LEFT_BRACKET = "(";
+        String RIGHT_BRACKET = ")";
+        File file = new File(uploadsDir, filename);
+        String newFilename = filename;
+        int i = 0;
+        while (file.exists()) {
+            newFilename = filename + LEFT_BRACKET + i + RIGHT_BRACKET;
+            file = new File(uploadsDir, newFilename);
+            i++;
+        }
+        return newFilename;
+    }
+}
